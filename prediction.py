14c4b0167dd5bcb43231ec84af2ba24885bab03b
Tan Ze Ying <63287098+TZY033@users.noreply.github.com>
2023-06-07 23:24:36 +0800

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import joblib
+
+
+def predict(data):
+    clf = joblib.load("rf_model.sav")
+    return clf.predict(data)
+
+
+# In[ ]:
+
+
+
+
